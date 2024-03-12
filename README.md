669fc3c729bc0bc86c1ceb00670829f7dcc2f06e
Dikshant <dikshant.073@gmail.com>
2024-03-12 15:12:19 +0530

    tried using cannonical mode for text editor

---
+++ README.md
@@ -0,0 +1,3 @@
+# ferrum_text
+Is a text editor built in rust for learning rust. It's rust implementation of [kilo](https://viewsourcecode.org/snaptoken/kilo/).
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use::std::io;
+use std::io::Read;
 fn main() {
-    println!("Hello, world!");
+    for b in io::stdin().bytes(){
+        let c = b.unwrap()as char;
+        println!("{}", c);
+
+        if c == 'q'{
+            break;
+        }
+    }
 }
