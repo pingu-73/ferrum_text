9bc13fdf0a15efbbca49b0cacd5a36adcffaaef0
Dikshant <dikshant.073@gmail.com>
2024-03-12 18:10:41 +0530

    converted terminal to raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use::std::io;
 use std::io::Read;
+
+use termion::raw::IntoRawMode;
+
 fn main() {
+    let _stdout = io::stdout().into_raw_mode().unwrap(); //binding raw mode (refer to Ownership rules)
+
     for b in io::stdin().bytes(){
         let c = b.unwrap()as char;
         println!("{}", c);
