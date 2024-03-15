3a77974b036264132210a8f58e5274935f451f43
Dikshant <dikshant.073@gmail.com>
2024-03-15 16:38:40 +0530

    moved terminal codes to Terminal, hidden that terminal is 1 based, prevented overflow in cursor_pos

--- src/editor.rs
+++ src/editor.rs
@@ -1,10 +1,5 @@
 use crate::Terminal;
-use::std::io;
-use::std::io::Write;
-
-use termion::raw::IntoRawMode;
 use termion::event::Key;
-use termion::input::TermRead;
 
 pub struct Editor {
     should_quit: bool,
@@ -13,8 +8,6 @@ pub struct Editor {
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = io::stdout().into_raw_mode().unwrap(); //binding raw mode (refer to Ownership rules)
-
         loop{
             if let Err(error) = self.refresh_screen(){
                 die(error);
@@ -36,20 +29,20 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error>{
-        // print!("\x1b[2J");
-        print!("{} {}", termion::clear::All, termion::cursor::Goto(1,1));
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
         if self.should_quit {
             println!("Goodbye\r");
         }
         else{
             self.draw_rows();
-            print!("{}", termion::cursor::Goto(1, 1));
+            Terminal::cursor_position(0, 0);
         }
-        io::stdout().flush()
+        Terminal::flush()
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error>{
-        let pressed_key = Self::read_key()?;
+        let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -62,17 +55,9 @@ impl Editor {
             println!("~\r");
         }
     }
-
-    fn read_key() -> Result<Key, std::io::Error> {
-        loop{
-            if let Some(key) = io::stdin().lock().keys().next() {
-                return  key;
-            }
-        }
-    }
 }
 
 fn die(e: std::io::Error){
-    print!("{}", termion::clear::All);
+    Terminal::clear_screen();
     panic!("{}", e);
 }
--- src/terminal.rs
+++ src/terminal.rs
@@ -1,3 +1,10 @@
+use::std::io;
+use::std::io::Write;
+
+use termion::raw::{IntoRawMode, RawTerminal};
+use termion::input::TermRead;
+use termion::event::Key;
+
 pub struct Size{
     pub width: u16,
     pub height: u16,
@@ -5,17 +12,42 @@ pub struct Size{
 
 pub struct Terminal{
     size: Size,     // using size fn to return size to avoid changing of size form outside.
+    _stdout: RawTerminal<io::Stdout>,
 }
 
 impl Terminal{
     pub fn default() -> Result<Self, std::io::Error> {
         let size = termion::terminal_size()?;
         Ok(Self {
-            size: Size { width: size.0, height: size.1 }
+            size: Size { width: size.0, height: size.1 },
+            _stdout: io::stdout().into_raw_mode()?,
         })
     }
 
     pub fn size(&self) -> &Size {
         &self.size
+    }
+
+    pub fn clear_screen() {
+        print!("{}", termion::clear::All);
+    }
+
+    pub fn cursor_position(x: u16, y: u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+
+        print!("{}", termion::cursor::Goto(x, y)); 
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key() -> Result<Key, std::io::Error>{
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
+        }
     }
 }
