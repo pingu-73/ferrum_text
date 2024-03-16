1fcc872a4964aea1e10a0dbe34e2a02d6ad4d489
Dikshant <dikshant.073@gmail.com>
2024-03-16 16:54:43 +0530

    added scrolling feature

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Is a text editor built in rust for learning rust. It's rust implementation of [kilo](https://viewsourcecode.org/snaptoken/kilo/).
 
 # To-Do:
-- Text viewer
-- Scrolling
+- ~~Text viewer~~
+- ~~Scrolling~~
 - Text Editor
 - Syntax highlighting
 - Search
--- src/editor.rs
+++ src/editor.rs
@@ -22,6 +22,7 @@ pub struct Editor {
     terminal: Terminal,
     cursor_position: Position,
     document: Document,
+    offset: Position,
 }
           
 impl Editor {
@@ -53,6 +54,7 @@ impl Editor {
             terminal: Terminal::default().expect("Failed to initilize terminal"),
             cursor_position: Position::default(),
             document,
+            offset: Position::default(),
         }
     }
 
@@ -65,7 +67,10 @@ impl Editor {
         }
         else{
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            });
         }
         Terminal::cursor_show();
         Terminal::flush()
@@ -74,9 +79,13 @@ impl Editor {
     fn move_cursor(&mut self, key: Key) {
         let Position {mut y, mut x} = self.cursor_position; 
         
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize; 
+        let height = self.document.len();
+        let width = if let Some(row) = self.document.row(y){
+            row.len()
+        }
+        else{
+            0
+        }; 
 
         match key {
             Key::Up => y = y.saturating_sub(1),
@@ -107,14 +116,34 @@ impl Editor {
             Key::Up | Key::Down | Key::Right | Key::Left | Key::PageDown | Key::PageUp | Key::End | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
+        self.scroll();
         Ok(())
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let offset = &mut self.offset;
+        if y < offset.y {
+            offset.y = y;
+        }
+        else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        }
+        else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
     }
 
     fn draw_rows(&self) {
         let height = self.terminal.size().height;
         for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if let Some(row) = self.document.row(terminal_row as usize)  {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y)  {
                 self.draw_row(row);
             }
             else if terminal_row == height / 3 && self.document.is_empty() {
@@ -127,8 +156,9 @@ impl Editor {
     }
 
     pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
         let row = row.render(start, end);
         println!("{}\r", row);
     }
